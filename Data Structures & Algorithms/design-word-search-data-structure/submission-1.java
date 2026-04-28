04df5758578832fb1999cdf82bb5a6fcb6f2c6ff
Zing302 <sbollini06@gmail.com>
2026-04-27 18:49:12 -0700

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.java
@@ -0,0 +1,49 @@
+public class TrieNode{
+    TrieNode[] children;
+    boolean isEnd;
+    public TrieNode(){
+        children=new TrieNode[26];
+        isEnd=false;
+    }
+}
+class WordDictionary {
+    TrieNode root;
+    public WordDictionary() {
+        root=new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr=root;
+        for(char c:word.toCharArray()){
+            if(curr.children[c-'a']==null){
+                curr.children[c-'a']=new TrieNode();
+            }
+            curr=curr.children[c-'a'];
+        }
+        curr.isEnd=true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word,root,0);
+    }
+
+    public boolean dfs(String word, TrieNode root, int level){
+        for(int i=level;i<word.length();i++){
+            char c=word.charAt(i);
+            if(c=='.'){
+                for(TrieNode child:root.children){
+                   if(child!=null && dfs(word,child,i+1)){
+                        return true;
+                   }
+                }
+                return false;
+            }else{
+                if(root.children[c-'a']==null){
+                    return false;
+                }
+                root=root.children[c-'a'];
+            }
+        }
+        return root.isEnd;
+    }
+}
