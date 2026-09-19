1dcb6d3e187bac1dbaded784b64992afa359a917
Zing302 <sbollini06@gmail.com>
2026-09-19 09:41:26 -0700

    Add: target-sum - submission-5

---
+++ Data Structures & Algorithms/target-sum/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sumTotal=sum(nums);
+        if((sumTotal+target)/2 > sumTotal || (sumTotal+target)/2 < 0 || (sumTotal+target)%2 != 0 ){
+            return 0;
+        }
+        int[] dp=new int[sumTotal+1];
+        dp[0]=1;
+        for(int num:nums){
+            for(int i=sumTotal;i>=num;i--){
+                if(dp[i-num] > 0){
+                    dp[i]+=dp[i-num];
+                }
+            }
+        }
+        return dp[(sumTotal+target)/2];
+    
+    }
+
+    private int sum(int[] nums){
+        int total=0;
+        for(int num:nums){
+            total+=num;
+        }
+        return total;
+    }
+}
